0dfaac3f033d77c63c3e0963bd0d0c99bb2d028e
sello200 <carlossello20@gmail.com>
2026-03-21 20:51:36 +0700

    Add program to count digits of a number

---
+++ JumlahAngka.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n, count = 0;
+
+    printf("Masukkan angka: ");
+    scanf("%d", &n);
+
+    if (n == 0) {
+        count = 1;
+    } else {
+        while (n != 0) {
+            n = n / 10;
+            count++;
+        }
+    }
+
+    printf("Jumlah digit = %d\n", count);
+}
